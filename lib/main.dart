92f937a9e40745770b4924a75a9c48142321a412
Jeremae Apacionado <jtapacionado1998@gmail.com>
2022-06-26 21:07:32 +0800

    log in user added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:simpletodo/ui/screens/home_page.dart';
+import 'package:simpletodo/ui/screens/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  static final String title = 'To Do';
+static final String title = 'To Do';
 
   @override
   Widget build(BuildContext context) => MaterialApp(
         debugShowCheckedModeBanner: false,
         title: title,
         theme: ThemeData(
+          textTheme: GoogleFonts.merriweatherTextTheme(Theme.of(context).textTheme).apply(
+            bodyColor: Colors.black,
+          ),
           primarySwatch: Colors.teal,
           scaffoldBackgroundColor: Color(0xFF73bfaf),
         ),
-        home: HomePage(),
+        home: LoginPage(),
       );
 }
---
+++ lib/ui/screens/login_page.dart
@@ -0,0 +1,147 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [ 
+          //greetings
+          Padding(padding: EdgeInsets.only(top: 70),
+          ),
+          Text(
+            'Welcome user',
+          style: GoogleFonts.merriweather(
+          fontSize: 20,
+          color: Color(0xFFafeddd)
+          ),
+          ),
+          SizedBox(height: 10),
+          Text('To Do Application',
+          style: TextStyle(
+          fontSize: 10,
+
+          ),),
+          SizedBox(height: 30),
+          //textfield username
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xFFe6f5f1),
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: TextField(
+                  style: TextStyle(
+                  fontSize: 15,),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText:'Username',
+                    
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+          //textfield password
+      Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xFFe6f5f1),
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: TextField(
+                  obscureText: true,
+                  style: TextStyle(
+                  fontSize: 15,),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText:'Password',
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          //login button
+          // body: Container(
+          //   alignment: Alignment.center,
+          //   padding: EdgeInsets.all(32),
+          //   child: ElevatedButton(
+          //     style: ElevatedButton.styleFrom(),
+          //     child: Text('LogIn'),
+          //     onPressed: () => ,
+          //     ),
+
+            
+          // ),
+
+           Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 25.0),
+             child: Center(
+              child: Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(color: Color(0xFF04523d),
+                borderRadius: BorderRadius.circular(12),),
+                child: Text(
+                  'Log In',
+                style: TextStyle(
+                color: Colors.white, 
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                ),
+                ),
+              ),
+          ),
+           ),
+          SizedBox(height: 20),
+
+           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+                Text('Dont have an account?', style: TextStyle(
+                color: Colors.black, 
+                fontWeight: FontWeight.bold
+                ),
+               ),
+
+               Text('  Register Now', 
+               style: TextStyle(
+                color: Colors.white, 
+                fontWeight: FontWeight.bold,
+
+                ),
+                ),
+             ],
+           )
+        ],
+        ),
+      ),
+      ),
+    );
+  }
+}
